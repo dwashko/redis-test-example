7ff450bb75c6e17f20eabb66da8b7fa6d41b5120
Dann S Washko <dwashko@gmti.gannett.com>
2016-06-08 16:14:53 -0400

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'redis-test'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures redis-test'
+long_description 'Installs/Configures redis-test'
+version          '0.1.0'
